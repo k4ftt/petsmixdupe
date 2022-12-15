4e2ba61320afcf708f3f79073ed81ff4fa64dad6
k4ftt <103969132+k4ftt@users.noreply.github.com>
2022-12-15 22:21:41 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -21,7 +21,7 @@ local Tab = Window:MakeTab({
    
    Tab:AddParagraph("TRADE SCAM: Working!","")
 
-   Tab:AddParagraph("Booth duping: Working!","")
+   Tab:AddParagraph("BOOTH DUPING: Working!","")
  
    local Section = Tab:AddSection({
         Name = "Update"   
@@ -145,7 +145,8 @@ local Tab = Window:MakeTab({
         PremiumOnly = false
     })
 
- Tab:AddLabel("You need to make a pet in your booth the click ON if someone buys It, you will get the gems and the pet will still be in your inventory")
+ Tab:AddLabel("You need to make a pet in your booth the click ON if someone")
+ Tab:AddLabel("buys It, you will get the gems and the pet will still be in your inventory")
 
 local Section = Tab:AddSection({
 	Name = "ON/OFF"
@@ -167,7 +168,8 @@ local Section = Tab:AddSection({
         end    
     })
 
-Tab:AddLabel("In This Section Everything you buy will be free and you both will get the gems But you can't buy something that worths 1B and you only have like 100M")
+Tab:AddLabel("In This Section Everything you buy will be free and you both")
+Tab:AddLabel("will get the gems But you can't buy something that worths 1B and you only have like 100M")
 
 local Section = Tab:AddSection({
 	Name = "ON/OFF"
